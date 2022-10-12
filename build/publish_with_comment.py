967081d17869d68cfec8b943f3c98794c325d3f0
Pelidyai <dima-pile@mail.ru>
2022-10-12 21:10:03 +0700

    Comment from console

--- build/publish_with_comment.py
+++ build/publish_with_comment.py
@@ -2,7 +2,7 @@ import sys
 from build_tools import publish_to_git_with_comment
 
 if __name__ == '__main__':
-    if len(sys.argv) != 1:
+    if len(sys.argv) != 2:
         print("[ERROR] invalid argument")
-    comment = sys.argv[0]
+    comment = sys.argv[1]
     publish_to_git_with_comment(comment)
