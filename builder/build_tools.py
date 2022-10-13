29976093681743ea27fc800c9eaa19f54b3983aa
Pelidyai <dima-pile@mail.ru>
2022-10-13 20:27:14 +0700

    Test commit#3 (publish improve)

--- builder/build_tools.py
+++ builder/build_tools.py
@@ -35,7 +35,7 @@ def publish_to_git():
               stdout=PIPE, stderr=STDOUT, universal_newlines=True, shell=True).stdout
     if out.__contains__('fatal'):
         sys(f'git checkout release/{version}')
-    sys(f'git rebase --onto {saved_branch}')
+    sys(f'git rebase {saved_branch}')
     sys(f'git stash pop')
     sys('git add ..')
     sys('git add .')
