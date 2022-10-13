9d15582c77a6feeed7671c2108bc182f8b334e18
Pelidyai <dima-pile@mail.ru>
2022-10-13 20:27:50 +0700

    Test commit#4 (publish improve)

--- builder/build_tools.py
+++ builder/build_tools.py
@@ -29,7 +29,7 @@ def get_my_name(url: str) -> str:
 def publish_to_git():
     version = get_my_version()
     saved_branch = run(f' git rev-parse --abbrev-ref HEAD',
-              stdout=PIPE, stderr=STDOUT, universal_newlines=True, shell=True).stdout
+              stdout=PIPE, stderr=STDOUT, universal_newlines=True, shell=True).stdout.replace('\n', '')
     sys(f'git stash')
     out = run(f'git switch -c release/{version}',
               stdout=PIPE, stderr=STDOUT, universal_newlines=True, shell=True).stdout
