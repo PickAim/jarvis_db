bcfbbd4c8f5154cad3aff834c675b9f3adbebbe8
Pelidyai <dima-pile@mail.ru>
2022-10-13 20:28:17 +0700

    [Auto: 2022-10-13 20:28:17.755768] publish 0.0.5

--- builder/build_tools.py
+++ builder/build_tools.py
@@ -35,6 +35,9 @@ def publish_to_git():
               stdout=PIPE, stderr=STDOUT, universal_newlines=True, shell=True).stdout
     if out.__contains__('fatal'):
         sys(f'git checkout release/{version}')
+
+
+        
     sys(f'git rebase {saved_branch}')
     sys(f'git stash pop')
     sys('git add ..')
