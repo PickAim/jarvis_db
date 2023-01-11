0ad37ef1cfe0be4c04e0e1a4b856a7c6f76f53a4
Sawyron <s4pakvv@gmail.com>
2023-01-11 22:14:21 +0700

    created repository templates

---
+++ jarvis_db/repositores/market/infrastructure.py
@@ -0,0 +1,18 @@
+from jorm.market.infrastructure import Niche
+
+class NicheRepository:
+    def addNiche(niche: Niche):
+        pass
+
+class CategoryRepository:
+    pass
+
+class AddressRepository:
+    pass
+
+class WarehouseRepository:
+    pass
+
+class MarketPlaceRepository:
+    pass
+
---
+++ jarvis_db/repositores/market/items.py
@@ -0,0 +1,12 @@
+class ProductHistoryRepository:
+    pass
+
+class ProductRepository:
+    pass
+
+class ClientProductRepository:
+    pass
+
+class MarketplaceProductRepository:
+    pass
+
