2a3789fbc22e1358b9dcf09299ad20595161ec12
Pelidyai <dima-pile@mail.ru>
2023-01-13 23:17:24 +0700

    Deps update, rename abstract methods declarations

--- jarvis_db/access/accessers.py
+++ jarvis_db/access/accessers.py
@@ -1,10 +1,23 @@
 from jarvis_calc.database_interactors.db_access import DBAccessProvider
-from jorm.market.person import Client
+
+from jorm.market.infrastructure import Warehouse, Niche
+from jorm.market.person import Client, User
 
 
 class ConcreteDBAccessProvider(DBAccessProvider):
-    def get_client(self) -> Client:
+    def get_current_user(self) -> User:
+        pass
+
+    def get_current_client(self) -> Client:
+        pass
+
+    def get_niche(self, niche_name: str) -> Niche:
+        pass
+
+    def get_warehouse(self, warehouse_name: str) -> Warehouse:
+        pass
+
+    def get_all_warehouses(self) -> list[Warehouse]:
         pass
 
     # TODO create me
-
