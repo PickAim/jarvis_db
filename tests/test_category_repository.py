aed58be3a870212b71f30a4f4d43a13cf65f1152
Sawyron <s4pakvv@gmail.com>
2023-01-21 23:26:16 +0700

    Added niche count assertion

--- tests/test_category_repository.py
+++ tests/test_category_repository.py
@@ -61,6 +61,8 @@ class CategoryRepositoryTest(unittest.TestCase):
             repository = CategoryRepository(session)
             categories = repository.fetch_all()
         self.assertEqual(len(categories), categories_to_add)
+        for category, expected_niches in zip(categories, niches_per_category):
+            self.assertEqual(len(category.niches), expected_niches)
 
 
 if __name__ == '__main__':
