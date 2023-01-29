fce2d0e241e839105f72089ffd07141b86c2ec4d
Pelidyai <dima-pile@mail.ru>
2023-01-29 16:27:50 +0700

    Deps update, change methods signature

--- jarvis_db/access/accessers.py
+++ jarvis_db/access/accessers.py
@@ -1,14 +1,17 @@
 from jarvis_calc.database_interactors.db_access import DBAccessProvider
 
 from jorm.market.infrastructure import Warehouse, Niche
-from jorm.market.person import Client, User
+from jorm.market.person import User, Account
 
 
 class ConcreteDBAccessProvider(DBAccessProvider):
-    def get_current_user(self) -> User:
+    def get_user_by_account(self, account: Account) -> User:
         pass
 
-    def get_current_client(self) -> Client:
+    def get_user_by_id(self, user_id: int) -> User:
+        pass
+
+    def get_account(self, login: str) -> Account:
         pass
 
     def get_niche(self, niche_name: str) -> Niche:
