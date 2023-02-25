9225f9d23343cbee993b9a27c9cc65e9280441e8
Sawyron <s4pakvv@gmail.com>
2023-02-25 16:20:14 +0700

    UserRepository: find_by_account is implemented

--- jarvis_db/repositores/market/person/user_repository.py
+++ jarvis_db/repositores/market/person/user_repository.py
@@ -1,6 +1,6 @@
 from typing import Iterable
 
-from jorm.market.person import User
+from jorm.market.person import Account, User
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
@@ -23,6 +23,14 @@ class UserRepository:
         db_user = self.__to_table_mapper.map(user)
         db_user.account_id = account_id
         self.__session.add(db_user)
+
+    def find_by_account(self, account: Account) -> User:
+        db_user = self.__session.execute(
+            select(tables.User)
+            .join(tables.User.account)
+            .where(tables.Account.email == account.email, tables.Account.phone == account.phone)
+        ).scalar_one()
+        return self.__to_jorm_mapper.map(db_user)
 
     def fetch_all(self) -> list[User]:
         db_users = self.__session.execute(
--- tests/repositories/test_user_repository.py
+++ tests/repositories/test_user_repository.py
@@ -8,6 +8,7 @@ from jarvis_db import tables
 from jarvis_db.db_config import Base
 from jarvis_db.repositores.mappers.market.person.user_mappers import UserJormToTableMapper, UserTableToJormMapper
 from jarvis_db.repositores.market.person import UserRepository
+from jorm.market.person import Account
 
 
 class UserRepositoryTest(unittest.TestCase):
@@ -15,17 +16,18 @@ class UserRepositoryTest(unittest.TestCase):
         engine = create_engine('sqlite://')
         session = sessionmaker(bind=engine, autoflush=False)
         Base.metadata.create_all(engine)
-        email = 'user@mail.org'
+        account = Account('1231', 'user@mail.org', 'qwerty')
         with session() as s, s.begin():
-            s.add(tables.Account(phone='1231',
-                  email='user@mail.org', password='safkm1'))
+            s.add(tables.Account(phone=account.phone,
+                  email=account.email, password=account.hashed_password))
         with session() as s:
             db_account = s.execute(
                 select(tables.Account)
-                .where(tables.Account.email == email)
+                .where(tables.Account.email == account.email)
             ).scalar_one()
             self.__account_id = db_account.id
         self.__session = session
+        self.__account = account
 
     def test_add(self):
         user = User(name='User#1')
@@ -40,3 +42,18 @@ class UserRepositoryTest(unittest.TestCase):
                 .where(tables.User.name == user.name, tables.Account.id == self.__account_id)
             ).scalar_one()
             self.assertEqual(user.name, db_user.name)
+
+    def test_find_by_account(self):
+        user_name = 'User#1'
+        with self.__session() as session, session.begin():
+            db_account = session.execute(
+                select(tables.Account)
+                .where(tables.Account.email == self.__account.email)
+            ).scalar_one()
+            session.add(tables.User(name=user_name,
+                        account=db_account, profit_tax=0))
+        with self.__session() as session:
+            repository = UserRepository(
+                session, UserTableToJormMapper(), UserJormToTableMapper())
+            user = repository.find_by_account(self.__account)
+            self.assertEqual(user_name, user.name)
