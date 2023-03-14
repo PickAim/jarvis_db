fcc313f3dd8c1fbd0c0ec7b0f440efa9febde007
Sawyron <s4pakvv@gmail.com>
2023-03-15 00:58:12 +0700

    EconomyRequestRepository: add id to return value

--- jarvis_db/repositores/market/service/economy_request_repository.py
+++ jarvis_db/repositores/market/service/economy_request_repository.py
@@ -17,22 +17,23 @@ class EconomyRequestRepository:
         self.__to_jorm_mapper = to_jorm_mapper
         self.__to_table_mapper = to_table_mapper
 
-    def add(self, request: EconomyRequest, user_id: int, marketplace_name: str):
+    def add(self, request: EconomyRequest, user_id: int, marketplace_id: int):
         db_request = self.__to_table_mapper.map(request)
         niche_id = self.__session.execute(
             select(tables.Niche.id)
             .join(tables.Niche.category)
             .join(tables.Category.marketplace)
-            .where(tables.Marketplace.name.ilike(marketplace_name))
+            .where(tables.Marketplace.id == marketplace_id)
             .where(tables.Niche.name.ilike(request.niche_name))
         ).scalar_one()
         db_request.niche_id = niche_id
+        db_request.user_id = user_id
         self.__session.add(db_request)
 
-    def fetch_user_requests(self, user_id: int) -> list[EconomyRequest]:
+    def fetch_user_requests(self, user_id: int) -> dict[int, EconomyRequest]:
         db_requests = self.__session.execute(
             select(tables.EconomyRequest)
             .join(tables.EconomyRequest.user)
             .where(tables.User.id == user_id)
         ).scalars().all()
-        return [self.__to_jorm_mapper.map(request) for request in db_requests]
+        return {request.id: self.__to_jorm_mapper.map(request) for request in db_requests}
