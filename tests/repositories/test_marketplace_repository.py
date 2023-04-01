d22b5d040ab99d86e0c224137c6f5a460119b240
Sawyron <s4pakvv@gmail.com>
2023-04-02 02:19:54 +0700

    imlement MarketplaceService

--- tests/repositories/test_marketplace_repository.py
+++ tests/repositories/test_marketplace_repository.py
@@ -19,35 +19,6 @@ class MarketplaceRepositoryTest(unittest.TestCase):
         Base.metadata.create_all(engine)
         self.__session = session
 
-    def test_add(self):
-        marketplace = Marketplace('marketplace1')
-        with self.__session() as session, session.begin():
-            repository = MarketplaceRepository(
-                session, MarketplaceTableToJormMapper(
-                    WarehouseTableToJormMapper()),
-                MarketplaceJormToTableMapper(WarehouseJormToTableMapper()))
-            repository.add(marketplace)
-        with self.__session() as session:
-            db_marketplace: tables.Marketplace = session.query(
-                tables.Marketplace).one()
-            self.assertEqual(marketplace.name, db_marketplace.name)
-
-    def test_add_all(self):
-        marketplaces_to_add = 10
-        marketplaces = [Marketplace(
-            f'marketplace_{i}') for i in range(1, marketplaces_to_add + 1)]
-        with self.__session() as session, session.begin():
-            repository = MarketplaceRepository(
-                session, MarketplaceTableToJormMapper(
-                    WarehouseTableToJormMapper()),
-                MarketplaceJormToTableMapper(WarehouseJormToTableMapper()))
-            repository.add_all(marketplaces)
-        with self.__session() as session:
-            db_marketplaces: list[tables.Marketplace] = session.query(
-                tables.Marketplace).all()
-            for jorm_marketplace, db_marketplace in zip(marketplaces, db_marketplaces, strict=True):
-                self.assertTrue(jorm_marketplace.name, db_marketplace.name)
-
     def test_fetch_all(self):
         marketplaces_to_add = 10
         with self.__session() as session, session.begin():
@@ -55,11 +26,8 @@ class MarketplaceRepositoryTest(unittest.TestCase):
                 name=f'markeplace_{i}') for i in range(1, marketplaces_to_add + 1)]
             session.add_all(db_marketplaces)
         with self.__session() as session:
-            repository = MarketplaceRepository(
-                session, MarketplaceTableToJormMapper(
-                    WarehouseTableToJormMapper()),
-                MarketplaceJormToTableMapper(WarehouseJormToTableMapper()))
-            marketplaces = repository.fetch_all()
+            repository = MarketplaceRepository(session)
+            marketplaces = repository.find_all()
             self.assertEqual(marketplaces_to_add, len(marketplaces))
 
 
---
+++ tests/services/test_marketplace_service.py
@@ -0,0 +1,49 @@
+import unittest
+
+from jorm.market.infrastructure import Marketplace as MarketplaceEntity
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from jarvis_db.repositores.mappers.market.infrastructure.marketplace_mappers import \
+    MarketplaceTableToJormMapper
+from jarvis_db.repositores.mappers.market.infrastructure.warehouse_mappers import \
+    WarehouseTableToJormMapper
+from jarvis_db.repositores.market.infrastructure.marketplace_repository import \
+    MarketplaceRepository
+from jarvis_db.services.market.infrastructure.marketplace_service import \
+    MarketplaceService
+from jarvis_db.tables import Marketplace
+from tests.db_context import DbContext
+
+
+class MarketplaceServiceTest(unittest.TestCase):
+    def setUp(self):
+        self.__db_context = DbContext()
+
+    def test_create(self):
+        marketplace_name = 'qwerty'
+        with self.__db_context.session() as session, session.begin():
+            service = create_service(session)
+            service.create(MarketplaceEntity(marketplace_name))
+        with self.__db_context.session() as session:
+            marketplace = session.execute(
+                select(Marketplace)
+                .where(Marketplace.name == marketplace_name)
+            ).scalar_one()
+            self.assertEqual(marketplace_name, marketplace.name)
+
+    def test_find_by_name(self):
+        marketplace_name = 'qwerty'
+        with self.__db_context.session() as session, session.begin():
+            session.add(Marketplace(name=marketplace_name))
+        with self.__db_context.session() as session:
+            service = create_service(session)
+            found, _ = service.find_by_name(marketplace_name)
+            self.assertEqual(marketplace_name, found.name)
+
+
+def create_service(session: Session) -> MarketplaceService:
+    return MarketplaceService(
+        MarketplaceRepository(session),
+        MarketplaceTableToJormMapper(WarehouseTableToJormMapper())
+    )
