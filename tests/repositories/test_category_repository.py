7b3bcc897825372b6975ab4ea37cf3505030a45d
Sawyron <s4pakvv@gmail.com>
2023-04-09 02:43:14 +0700

    implement CategoryService

--- tests/repositories/test_category_repository.py
+++ tests/repositories/test_category_repository.py
@@ -1,112 +1,28 @@
 import unittest
+from jarvis_db.repositores.market.infrastructure.category_repository import CategoryRepository
 
-from jorm.market.infrastructure import Category, HandlerType, Niche
-from sqlalchemy import create_engine, select
-from sqlalchemy.orm import sessionmaker
-
-from jarvis_db import tables
-from jarvis_db.db_config import Base
-from jarvis_db.repositores.mappers.market.infrastructure import (
-    CategoryJormToTableMapper, CategoryTableToJormMapper,
-    NicheJormToTableMapper, NicheTableToJormMapper)
-from jarvis_db.repositores.market.infrastructure import CategoryRepository
+from jarvis_db.tables import Category, Marketplace
+from tests.db_context import DbContext
 
 
 class CategoryRepositoryTest(unittest.TestCase):
     def setUp(self) -> None:
-        engine = create_engine('sqlite://')
-        session = sessionmaker(bind=engine, autoflush=False)
-        Base.metadata.create_all(engine)
+        self.__db_context = DbContext()
         marketplace_id = 1
-        with session() as s, s.begin():
-            s.add(tables.Marketplace(id=marketplace_id, name='marketplace1'))
-        self.__session = session
+        with self.__db_context.session() as s, s.begin():
+            s.add(Marketplace(id=marketplace_id, name='marketplace1'))
         self.__marketplace_id = marketplace_id
 
-    def test_add(self):
-        niches_count = 10
-        category_name = 'cat1'
-        niches = [
-            Niche(f'n{i}',
-                  {commission: 0.1 * index for index,
-                  commission in enumerate(list(HandlerType))},
-                  i * 0.2)
-            for i in range(niches_count)]
-        category = Category(category_name, {
-            niche.name: niche for niche in niches
-        })
-        with self.__session() as session, session.begin():
-            repository = CategoryRepository(session, CategoryTableToJormMapper(
-                NicheTableToJormMapper()), CategoryJormToTableMapper(NicheJormToTableMapper()))
-            repository.add(
-                category, self.__marketplace_id)
-        with self.__session() as session:
-            db_category = session.execute(
-                select(tables.Category)
-                .join(tables.Category.niches)
-                .filter(tables.Category.name == category_name)
-                .distinct()
-            ).scalar_one()
-            self.assertEqual(db_category.name, category_name)
-            for niche, db_niche in zip(niches, db_category.niches, strict=True):
-                self.assertEqual(niche.name, db_niche.name)
-
-    def test_add_all(self):
-        categories_to_add = 10
-        categories = [Category(f'category_{i}', {})
-                      for i in range(1, categories_to_add + 1)]
-        with self.__session() as session, session.begin():
-            repository = CategoryRepository(
-                session, CategoryTableToJormMapper(NicheTableToJormMapper()),
-                CategoryJormToTableMapper(NicheJormToTableMapper()))
-            repository.add_all(
-                categories, self.__marketplace_id)
-        with self.__session() as session:
-            db_categories = session.execute(
-                select(tables.Category)
-            ).scalars().all()
-            for jorm_category, db_category in zip(categories, db_categories, strict=True):
-                self.assertEqual(jorm_category.name, db_category.name)
-
     def test_find_by_name(self):
-        category_name = 'category_1'
-        with self.__session() as session, session.begin():
-            db_category = tables.Category(
-                name=category_name, marketplace_id=self.__marketplace_id)
-            session.add(db_category)
-        with self.__session() as session:
-            repository = CategoryRepository(session, CategoryTableToJormMapper(
-                NicheTableToJormMapper()), CategoryJormToTableMapper(NicheJormToTableMapper()))
-            category, _ = repository.find_by_name(
+        category_name = 'qwerty'
+        with self.__db_context.session() as session, session.begin():
+            session.add(
+                Category(name=category_name, marketplace_id=self.__marketplace_id))
+        with self.__db_context.session() as session:
+            repository = CategoryRepository(session)
+            found = repository.find_by_name(
                 category_name, self.__marketplace_id)
-            self.assertEqual(category_name, category.name)
-
-    def test_fetch_all(self):
-        categories_to_add = 10
-        niches_per_category = [i + 1 for i in range(categories_to_add)]
-        db_categories = [tables.Category(
-            name=f'cat{i}',
-            niches=[tables.Niche(
-                name=f'niche_cat{i}_n_{j}',
-                marketplace_commission=j * 10,
-                client_commission=j * 20,
-                partial_client_commission=j * 30,
-                return_percent=(j + 1) * 10,
-            )
-                for j in range(item)],
-            marketplace_id=self.__marketplace_id
-        )
-            for i, item in enumerate(niches_per_category)]
-        with self.__session() as session, session.begin():
-            session.add_all(db_categories)
-        with self.__session() as session:
-            repository = CategoryRepository(session, CategoryTableToJormMapper(
-                NicheTableToJormMapper()), CategoryJormToTableMapper(NicheJormToTableMapper()))
-            categories = repository.find_all(
-                self.__marketplace_id)
-        self.assertEqual(len(categories), categories_to_add)
-        for category, expected_niches in zip(categories.values(), niches_per_category, strict=True):
-            self.assertEqual(len(category.niches), expected_niches)
+            self.assertEqual(category_name, found.name)
 
 
 if __name__ == '__main__':
