0c227f1a520c7d6bbc5d0423413d626da17345d1
Sawyron <s4pakvv@gmail.com>
2023-04-12 23:42:14 +0700

    implement new jorm methods

--- jarvis_db/repositores/market/person/account_repository.py
+++ jarvis_db/repositores/market/person/account_repository.py
@@ -12,6 +12,12 @@ class AccountRepository(AlchemyRepository[Account]):
             .where(Account.email == email)
         ).scalar_one()
 
+    def find_by_phone(self, phone: str) -> Account:
+        return self._session.execute(
+            select(Account)
+            .where(Account.phone == phone)
+        ).scalar_one()
+
     def find_all(self) -> list[Account]:
         db_accounts = self._session.execute(
             select(Account)
--- jarvis_db/services/market/person/account_service.py
+++ jarvis_db/services/market/person/account_service.py
@@ -26,6 +26,10 @@ class AccountService:
         account = self.__account_repository.find_by_email(email)
         return self.__table_mapper.map(account), account.id
 
+    def find_by_phone(self, phone: str) -> tuple[AccountEntity, int]:
+        account = self.__account_repository.find_by_phone(phone)
+        return self.__table_mapper.map(account), account.id
+
     def find_all(self) -> dict[int, AccountEntity]:
         accounts = self.__account_repository.find_all()
         return {account.id: self.__table_mapper.map(account) for account in accounts}
