a7fee6a90a5b327ba9b080fd1e4a63d04a859ef7
Sawyron <s4pakvv@gmail.com>
2023-06-14 00:45:56 +0700

    remove empty string check

--- jarvis_db/repositores/market/person/account_repository.py
+++ jarvis_db/repositores/market/person/account_repository.py
@@ -7,12 +7,12 @@ from jarvis_db.tables import Account
 class AccountRepository(AlchemyRepository[Account]):
     def find_by_email(self, email: str) -> Account | None:
         return self._session.execute(
-            select(Account).where(Account.email == email).where(Account.email != "")
+            select(Account).where(Account.email == email)
         ).scalar_one_or_none()
 
     def find_by_phone(self, phone: str) -> Account | None:
         return self._session.execute(
-            select(Account).where(Account.phone == phone).where(Account.phone != "")
+            select(Account).where(Account.phone == phone)
         ).scalar_one_or_none()
 
     def find_by_email_or_phone(self, email: str, phone: str) -> Account | None:
