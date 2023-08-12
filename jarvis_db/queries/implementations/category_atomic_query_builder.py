47eabd570b6e64888d7083c7989a1dccb991d0d0
Sawyron <s4pakvv@gmail.com>
2023-08-12 23:09:24 +0700

    implement join methods

--- jarvis_db/queries/implementations/category_atomic_query_builder.py
+++ jarvis_db/queries/implementations/category_atomic_query_builder.py
@@ -1,15 +1,19 @@
 from typing import Iterable
 
+from sqlalchemy import Select
 from sqlalchemy.orm import joinedload
 from sqlalchemy.orm.strategy_options import _AbstractLoad
 
-from jarvis_db.queries.query_builder import QueryBuilder
+from jarvis_db.queries.query_builder import _S, QueryBuilder
 from jarvis_db.schemas import Category, Niche
 
 
 class CategoryAtomicJoinQueryBuilder(QueryBuilder[Category]):
     def __init__(self, niche_query_builder: QueryBuilder[Niche]):
         self.__niche_query_builder = niche_query_builder
+
+    def join(self, query: Select[tuple[_S]]) -> Select[tuple[_S]]:
+        return query.join(Category.niches)
 
     def provide_load_options(self) -> Iterable[_AbstractLoad]:
         return [
--- jarvis_db/queries/implementations/product_atomic_query_builder.py
+++ jarvis_db/queries/implementations/product_atomic_query_builder.py
@@ -1,13 +1,21 @@
 from typing import Iterable
 
+from sqlalchemy import Select
 from sqlalchemy.orm import joinedload
 from sqlalchemy.orm.strategy_options import _AbstractLoad
 
-from jarvis_db.queries.query_builder import QueryBuilder
+from jarvis_db.queries.query_builder import _S, QueryBuilder
 from jarvis_db.schemas import Leftover, ProductCard, ProductHistory
 
 
 class ProductCardAtomicJoinQueryBuilder(QueryBuilder[ProductCard]):
+    def join(self, query: Select[tuple[_S]]) -> Select[tuple[_S]]:
+        return (
+            query.outerjoin(ProductCard.histories)
+            .outerjoin(ProductHistory.leftovers)
+            .outerjoin(Leftover.warehouse)
+        )
+
     def provide_load_options(self) -> Iterable[_AbstractLoad]:
         return [
             joinedload(ProductCard.histories)
