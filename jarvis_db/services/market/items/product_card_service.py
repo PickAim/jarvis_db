e436151a3938dcf1aaac4e57700278408e78c315
Sawyron <s4pakvv@gmail.com>
2023-08-20 14:23:44 +0700

    cleanup

--- jarvis_db/services/market/items/product_card_service.py
+++ jarvis_db/services/market/items/product_card_service.py
@@ -5,7 +5,7 @@ from sqlalchemy import select, update
 from sqlalchemy.orm import Session, noload, joinedload
 
 from jarvis_db.core.mapper import Mapper
-from jarvis_db.schemas import Category, Leftover, Niche, ProductCard, ProductHistory
+from jarvis_db.schemas import Leftover, Niche, ProductCard, ProductHistory
 from jarvis_db.services.market.items.product_history_service import (
     ProductHistoryService,
 )
