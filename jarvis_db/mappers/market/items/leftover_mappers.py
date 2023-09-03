8f2ced794dc9c48df7c0de89fa261754bcdcf5ff
Sawyron <s4pakvv@gmail.com>
2023-09-03 23:10:36 +0700

    LeftoverJormToTableMapper - replace loop with comprehation

--- jarvis_db/mappers/market/items/leftover_mappers.py
+++ jarvis_db/mappers/market/items/leftover_mappers.py
@@ -4,12 +4,12 @@ from typing import Iterable
 from jorm.market.items import StorageDict
 from jorm.support.types import SpecifiedLeftover
 
-from jarvis_db import schemas
 from jarvis_db.core.mapper import Mapper
+from jarvis_db.schemas import Leftover
 
 
-class LeftoverTableToJormMapper(Mapper[Iterable[schemas.Leftover], StorageDict]):
-    def map(self, value: Iterable[schemas.Leftover]) -> StorageDict:
+class LeftoverTableToJormMapper(Mapper[Iterable[Leftover], StorageDict]):
+    def map(self, value: Iterable[Leftover]) -> StorageDict:
         return StorageDict(
             {
                 gid: [
@@ -23,12 +23,10 @@ class LeftoverTableToJormMapper(Mapper[Iterable[schemas.Leftover], StorageDict])
         )
 
 
-class LeftoverJormToTableMapper(Mapper[StorageDict, Iterable[schemas.Leftover]]):
-    def map(self, value: StorageDict) -> Iterable[schemas.Leftover]:
-        result = []
-        for _, leftovers in value.items():
-            for leftover in leftovers:
-                result.append(
-                    schemas.Leftover(type=leftover.specify, quantity=leftover.leftover)
-                )
-        return result
+class LeftoverJormToTableMapper(Mapper[StorageDict, Iterable[Leftover]]):
+    def map(self, value: StorageDict) -> Iterable[Leftover]:
+        return [
+            Leftover(type=leftover.specify, quantity=leftover.leftover)
+            for _, leftovers in value.items()
+            for leftover in leftovers
+        ]
